3b1edff56ac4b377ebbb837adf54bdf682ec00fb
naag <nagendra.kakarla@concur.com>
2017-04-02 18:48:05 -0500

    Adding spring boot semantics

--- src/main/java/com/rilussion/alpha/controller/Controller.java
+++ src/main/java/com/rilussion/alpha/controller/Controller.java
@@ -1,4 +1,4 @@
-package controller;
+package com.rilussion.alpha.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
