9fe82893277180ae0198a1f3b6370228e5d99391
naag <nagendra.kakarla@concur.com>
2017-04-02 18:45:44 -0500

    Adding spring boot semantics

---
+++ src/main/java/com/rilussion/alpha/controller/Controller.java
@@ -0,0 +1,21 @@
+package controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by nkakarla on 4/2/17.
+ */
+
+@RestController
+@RequestMapping("/tickers")
+public class Controller {
+
+    @RequestMapping(value = "/{ticker}", method = RequestMethod.GET)
+    public String getTicker(@PathVariable String ticker){
+
+        return "Alphabet";
+    }
+}
