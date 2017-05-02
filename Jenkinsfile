96eda30bc2e2bd2b2ffc683495e4bf304267c332
naag <nagendra.kakarla@concur.com>
2017-05-02 14:27:15 -0500

    Examining parallel run in pipelines

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,25 +5,32 @@ node {
         deleteDir()
         checkout scm
     }
+
     stage('build') {
         sh './gradlew build -x test'
     }
-    stage('Unit tests') {
-        sh './gradlew -Dtest.single=AppTest test'
+
+    parallel quality:{
+        stage('Unit tests') {
+            sh './gradlew -Dtest.single=AppTest test'
+        }
+        stage('code analysis and coco') {
+            def g = tool 'GRADL'
+            env.PATH = "${g}/bin:${env.path}"
+            sh 'gradle sonarqube -x test'
+        }
     }
-    stage('code analysis and coco') {
-        def g = tool 'GRADL'
-        env.PATH = "${g}/bin:${env.path}"
-        sh 'gradle sonarqube -x test'
-    }
+
     stage('dockerize-remove') {
 
         sh './gradlew composeDown'
     }
+
     stage('dockerize & compose app server and mock server ') {
 
         sh './gradlew composeUp'
     }
+    
     stage('Integration Test') {
 
         sh './gradlew -Dtest.single=AppIntegrationTest test'
--- src/test/groovy/AppTest.groovy
+++ src/test/groovy/AppTest.groovy
@@ -16,4 +16,69 @@ class AppTest extends Specification {
         then:
         result != null
     }
+
+    def "application has a greeting1"() {
+        setup:
+        def app = new Application()
+        and:
+            Thread.sleep 3100
+
+        when:
+        def result = app
+
+        then:
+        result != null
+    }
+
+    def "application has a greeting2"() {
+        setup:
+        def app = new Application()
+        and:
+        Thread.sleep 4100
+
+        when:
+        def result = app
+
+        then:
+        result != null
+    }
+
+    def "application has a greeting3"() {
+        setup:
+        def app = new Application()
+        and:
+        Thread.sleep 5100
+
+        when:
+        def result = app
+
+        then:
+        result != null
+    }
+
+    def "application has a greeting4"() {
+        setup:
+        def app = new Application()
+        and:
+        Thread.sleep 6100
+
+        when:
+        def result = app
+
+        then:
+        result != null
+    }
+
+    def "application has a greeting5"() {
+        setup:
+        def app = new Application()
+        and:
+        Thread.sleep 7100
+
+        when:
+        def result = app
+
+        then:
+        result != null
+    }
 }
